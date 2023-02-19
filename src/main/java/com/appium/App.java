6b34faae31fa8af82960dd521eec32eab76cc3b0
wellington_197 <fcowellcosta@gmail.com>
2023-02-19 12:07:56 -0300

    Correção da importação das configurações, e criado a máquina virtual AVD para executar projeto. Correção das variáveis de ambiente do projeto

--- src/main/java/com/appium/App.java
+++ src/main/java/com/appium/App.java
@@ -1,15 +1,31 @@
 package com.appium;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.MobilePlatform;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main( String[] args ) throws MalformedURLException {
+        /*Passar o caminho do arquivo APK*/
+        File apk=new File("C:\\xampp\\htdocs\\CURSOS_WELLINGTON\\CURSOS_UDEMY\\Appium_Test\\src\\main\\resources\\alura_esporte.apk");
+
+        /*Passar as configurações para appium executar*/
+        DesiredCapabilities configuracoes=new DesiredCapabilities();
+
+        configuracoes.setCapability(MobileCapabilityType.APP,apk.getAbsolutePath());
+        configuracoes.setCapability(MobileCapabilityType.PLATFORM_NAME, MobilePlatform.ANDROID);
+        configuracoes.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+
+        //Chamar a conexão com a URL do servidor executado no Powershel
+        URL urlConexao=new URL("http://127.0.0.1:4723/wd/hub");
+
+        //Criando nova conexão de driver
+        AppiumDriver driver=new AppiumDriver<>(urlConexao,configuracoes);
     }
 }
